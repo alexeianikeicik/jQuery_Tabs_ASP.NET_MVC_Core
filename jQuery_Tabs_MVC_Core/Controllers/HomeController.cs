9344ef192b61abdab753a0aebb41578a72220641
Алексей Аникейчик <cr3dostudio@gmail.com>
2022-06-22 11:02:25 +0300

    Добавьте файлы проекта.

---
+++ jQuery_Tabs_MVC_Core/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using jQuery_Tabs_MVC_Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jQuery_Tabs_MVC_Core.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(int selectedTab)
+        {
+            ViewBag.SelectedTab = selectedTab;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ jQuery_Tabs_MVC_Core/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace jQuery_Tabs_MVC_Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
